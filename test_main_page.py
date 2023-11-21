bf74e8ed247c65b52f28c376b70eae46de2b34d6
Mila_Mila <Solnishko2006-87@mail.ru>
2023-11-21 21:41:38 +0300

    тест в рамках реализации паттерна Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
-#команда для запуска теста: pytest -v --tb=line --language=en test_main_page.py
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser): #команда для запуска теста: pytest -v --tb=line --language=en test_main_page.py
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
