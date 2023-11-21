52fdc184ef0f1ff4da3863b47c1c7d600dee5769
Mila_Mila <Solnishko2006-87@mail.ru>
2023-11-21 23:44:45 +0300

    добавлена обработка исключений

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,26 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser, url):
+    """общие методы для работы с браузером"""
+    def __init__(self, browser: WebDriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
+        """открываем ссылку"""
         self.browser.get(self.url)
+
+    def is_element_present(self,how,what)-> bool:
+        """метод для перехватыва исключений.
+        Два аргумента: как искать (css, id, xpath и тд) и что искать (строку-селектор)"""
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+
+
